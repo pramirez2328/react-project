06fd41c16bca1e173190ca7df5a6e132603ef193
ramirez <prlara2000@gmail.com>
2023-07-03 18:09:15 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -7,7 +11,12 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='about' element={<AboutPage />} />
+      </Routes>
       <Footer />
     </div>
   );
