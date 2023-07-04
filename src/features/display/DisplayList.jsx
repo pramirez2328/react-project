9558c705b06c67eba258b651bb94c24c7dacc853
ramirez <prlara2000@gmail.com>
2023-07-03 19:00:42 -0500

    add home page

---
+++ src/features/display/DisplayList.jsx
@@ -0,0 +1,24 @@
+import { Col, Row } from 'reactstrap';
+import DisplayCard from './DisplayCard';
+import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
+import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+
+const DisplayList = () => {
+  console.log(selectFeaturedCampsite);
+  console.log(selectFeaturedPromotion);
+  const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+
+  return (
+    <Row>
+      {items.map((item, idx) => {
+        return (
+          <Col md className='m-1' key={idx}>
+            <DisplayCard item={item} />
+          </Col>
+        );
+      })}
+    </Row>
+  );
+};
+
+export default DisplayList;
