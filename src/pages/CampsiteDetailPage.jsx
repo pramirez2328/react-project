f62c5e03d08ea362bb0ff88ae3989512455dfcb8
ramirez <prlara2000@gmail.com>
2023-07-08 00:36:56 -0500

    add subheader

--- src/pages/CampsiteDetailPage.jsx
+++ src/pages/CampsiteDetailPage.jsx
@@ -1,17 +1,19 @@
 import { Container, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
+import SubHeader from '../components/SubHeader';
 import CommentsList from '../features/comments/CommentsList';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 
 const CampsiteDetailPage = () => {
   const { campsiteId } = useParams();
-  console.log('id', campsiteId);
+
   const campsite = selectCampsiteById(campsiteId);
   console.log(campsite);
 
   return (
     <Container>
+      <SubHeader current={campsite.name} detail={true} />
       <Row>
         <CampsiteDetail campsite={campsite} />
         <CommentsList campsiteId={campsiteId} />
